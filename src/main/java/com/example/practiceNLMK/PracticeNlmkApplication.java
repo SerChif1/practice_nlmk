8b5a1e3aaa6be57401b31f2d64bc3d2d9b6baee1
Sergei_Chafonov <s.chafonov@mk.dunise.net>
2024-12-12 09:24:38 +0300

    first commit

---
+++ src/main/java/com/example/practiceNLMK/PracticeNlmkApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceNLMK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNlmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeNlmkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practiceNLMK/PracticeNlmkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceNLMK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeNlmkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
